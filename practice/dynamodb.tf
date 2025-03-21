cb37c66910f026f2990aeaa5a186b9766d00bd4a
Jyo-Patil <jyopatil221@gmail.com>
2025-03-21 09:23:57 +0000

    added code for dynamodb

--- practice/dynamodb.tf
+++ practice/dynamodb.tf
@@ -25,3 +25,57 @@ resource "aws_dynamodb_table" "Customers" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table_item" "Deposits" {
+  table_name = aws_dynamodb_table.Deposits.name
+  hash_key   = aws_dynamodb_table.Deposits.hash_key
+
+  item = <<ITEM
+{
+  "username": {"S": "nmh"},
+  "accountnumber": {"N": "11189"},
+  "date": {"S": "23-09-2024"},
+  "depositamount": {"N": "700"},
+  "deposittype": {"S": "saving"}
+}
+ITEM
+}
+
+resource "aws_dynamodb_table" "Deposits" {
+  name           = "Deposits"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "username"
+
+  attribute {
+    name = "username"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table_item" "Withdrawals" {
+  table_name = aws_dynamodb_table.Withdrawals.name
+  hash_key   = aws_dynamodb_table.Withdrawals.hash_key
+
+  item = <<ITEM
+{
+  "username": {"S": "xyz"},
+  "accountnumber": {"N": "111456"},
+  "date": {"S": "28-09-2024"},
+  "withdralamount": {"N": "800"},
+  "Withdrawltype": {"S": "Online"}
+}
+ITEM
+}
+
+resource "aws_dynamodb_table" "Withdrawals" {
+  name           = "Withdrawltype"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "username"
+
+  attribute {
+    name = "username"
+    type = "S"
+  }
+}
