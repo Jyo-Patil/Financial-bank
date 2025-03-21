43aefb3e87dd67baac5e954d17bd06c1809c71ef
Jyo-Patil <jyopatil221@gmail.com>
2025-03-21 12:39:15 +0000

    final code added

--- Infra/ecr.tf
+++ Infra/ecr.tf
@@ -1,8 +1,8 @@
-/*resource "aws_ecr_repository" "foo" {
+resource "aws_ecr_repository" "foo" {
   name                 = "bankapp"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
-}*/
+}
