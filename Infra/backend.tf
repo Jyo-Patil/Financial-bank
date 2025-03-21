85e11aa9f151caee1d77ba103346b69577dd6b90
Jyo-Patil <jyopatil221@gmail.com>
2025-03-21 11:53:09 +0000

    final code added

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "ecsbackendbucket"
+    bucket = "pythonbackendappbucket"
     key    = "terraform.tfstate"
     region = "us-east-1"
     encrypt = true
-    dynamodb_table = "ecstable"
     }
   }
 
