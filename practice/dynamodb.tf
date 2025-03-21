06478019d7d2db38458a7f56ac567a4727b088b5
Jyo-Patil <jyopatil221@gmail.com>
2025-03-21 09:08:24 +0000

    added code for dynamodb

--- practice/dynamodb.tf
+++ practice/dynamodb.tf
@@ -1,123 +1,27 @@
-resource "aws_dynamodb_table" "withdrawals" {
-  name         = "Withdrawals"
-  billing_mode = "PAY_PER_REQUEST" # Default settings with on-demand billing
+resource "aws_dynamodb_table_item" "Customers" {
+  table_name = aws_dynamodb_table.Customers.name
+  hash_key   = aws_dynamodb_table.Customers.hash_key
 
-  # Partition Key
-  attribute {
-    name = ["username"]
-    type = "S" # String
-  }
-
-  # Additional Attributes
-  attribute {
-    name = ["accountnumber"]
-    type = "N" # Number
-  }
-
-  attribute {
-    name = ["date"]
-    type = "S" # String
-  }
-
-  attribute {
-    name = ["withdrawlamount"]
-    type = "N" # Number
-  }
-
-  attribute {
-    name = ["withdrawltype"]
-    type = "S" # String
-  }
-
-  hash_key = ["username"]
-
-  tags = {
-    Name        = "Withdrawals"
-    Environment = "Production"
-  }
+  item = <<ITEM
+{
+  "exampleHashKey": {"S": "username"},
+  "accountnumber": {"N": "11111"},
+  "balance": {"N": "22222"},
+  "branch": {"S": "bgm"},
+  "password": {"S": "four"},
+  "phonenumber": {"N": "2222245685"}
+}
+ITEM
 }
 
-# Table: Deposits
-resource "aws_dynamodb_table" "deposits" {
-  name         = "Deposits"
-  billing_mode = "PAY_PER_REQUEST"
-
-  # Partition Key
-  attribute {
-    name = ["username"]
-    type = "S" # String
-  }
-
-  # Additional Attributes
-  attribute {
-    name = ["accountnumber"]
-    type = "N" # Number
-  }
+resource "aws_dynamodb_table" "Customers" {
+  name           = "Customers"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "username"
 
   attribute {
-    name = ["date"]
-    type = "S" # String
-  }
-
-  attribute {
-    name = ["depositamount"]
-    type = "N" # Number
-  }
-
-  attribute {
-    name = ["deposittype"]
-    type = "S" # String
-  }
-
-  hash_key = "username"
-
-  tags = {
-    Name        = "Deposits"
-    Environment = "Production"
-  }
-}
-
-# Table: Customers
-resource "aws_dynamodb_table" "customers" {
-  name         = "Customers"
-  billing_mode = "PAY_PER_REQUEST"
-
-  # Partition Key
-  attribute {
-    name = ["username"]
-    type = "S" # String
-  }
-
-  # Additional Attributes
-  attribute {
-    name = ["accountnumber"]
-    type = "N" # Number
-  }
-
-  attribute {
-    name = ["balance"]
-    type = "N" # Number
-  }
-
-  attribute {
-    name = ["branch"]
-    type = "S" # String
-  }
-
-  attribute {
-    name = ["password"]
-    type = "S" # String
-  }
-
-  attribute {
-    name = ["phonenumber"]
-    type = "N" # Number
-  }
-
-  hash_key = ["username"]
-
-  tags = {
-    Name        = "Customers"
-    Environment = "Production"
+    name = "username"
+    type = "S"
   }
 }
